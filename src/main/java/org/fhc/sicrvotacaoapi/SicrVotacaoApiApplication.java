f4affa1ffe24c437a1e02e707a6d01ba96ff0c7b
fcardoso <fcardoso@teste.com.br>
2026-03-13 19:23:44 -0300

    Initial commit: scaffolding basico com config. DB (Docker)

---
+++ src/main/java/org/fhc/sicrvotacaoapi/SicrVotacaoApiApplication.java
@@ -0,0 +1,13 @@
+package org.fhc.sicrvotacaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicrVotacaoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SicrVotacaoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fhc/sicrvotacaoapi/SicrVotacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fhc.sicrvotacaoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicrVotacaoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
